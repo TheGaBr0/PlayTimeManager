30265d1519e242e02768faf7f0bc35da2aea92a2
TheGaBro <gbottani19@gmail.com>
2025-02-18 12:26:54 +0100

    PlaytimePercentage now accepts multiple time formats as input

--- src/main/java/me/thegabro/playtimemanager/Commands/PlaytimePercentage.java
+++ src/main/java/me/thegabro/playtimemanager/Commands/PlaytimePercentage.java
@@ -1,6 +1,7 @@
 package me.thegabro.playtimemanager.Commands;
 
 import me.thegabro.playtimemanager.PlayTimeManager;
+import me.thegabro.playtimemanager.Users.OnlineUsersManager;
 import me.thegabro.playtimemanager.Utils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -13,35 +14,29 @@ import java.text.DecimalFormat;
 public class PlaytimePercentage implements CommandExecutor {
 
     private final PlayTimeManager plugin = PlayTimeManager.getInstance();
+    private final OnlineUsersManager onlineUsersManager = OnlineUsersManager.getInstance();
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String s, @NotNull String[] args) {
 
         if (sender.hasPermission("playtime.percentage")) {
             if(args.length > 0) {
-                long time, timeToTicks;
-                //rimpiazza tutti i numeri con ""
-                try{
-                    time = Integer.parseInt(args[0].replaceAll("[^\\d.]", ""));
-                } catch(NumberFormatException e) {
+
+                long timeToTicks = Utils.formattedPlaytimeToTicks(args[0]);
+
+                if(timeToTicks == -1L){
                     sender.sendMessage(Utils.parseColors(plugin.getConfiguration().getPluginPrefix() + " Time is not specified correctly!"));
                     return false;
                 }
-                String format = args[0].replaceAll("\\d", "");
-                switch(format) {
-                    case "d": timeToTicks = time * 1728000L; break;
-                    case "h": timeToTicks = time * 72000L; break;
-                    case "m": timeToTicks = time * 1200L; break;
-                    default:
-                        sender.sendMessage(Utils.parseColors(plugin.getConfiguration().getPluginPrefix() + " Time format must be specified! [d/h/m]"));
-                        return false;
-                }
+
                 DecimalFormat df = new DecimalFormat("#.##");
                 df.setRoundingMode(RoundingMode.HALF_UP);
+                onlineUsersManager.updateAllOnlineUsersPlaytime();
                 Object[] result = plugin.getDatabase().getPercentageOfPlayers(timeToTicks);
                 String formattedNumber = df.format(result[0]);
                 sender.sendMessage(Utils.parseColors(plugin.getConfiguration().getPluginPrefix() + " The players with playtime greater than or equal to §6" + args[0] +
                         " §7are §6" + result[1] + " §7and represent §6" + formattedNumber + "% §7of the §6" + result[2] + " §7players stored"));
+                return true;
             } else {
                 sender.sendMessage(Utils.parseColors(plugin.getConfiguration().getPluginPrefix() + " Missing arguments"));
             }
